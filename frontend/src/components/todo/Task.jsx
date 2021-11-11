bb36f7ae8f755144c75ba90cd91ed7a05a81562d
Nolan Seokane <nolan.seokane@wunderthompson.com>
2021-11-11 16:01:47 +0200

    Delete task mutation

--- frontend/src/components/todo/Task.jsx
+++ frontend/src/components/todo/Task.jsx
@@ -3,10 +3,19 @@ import React from 'react';
 // Styling
 import { task__wrapper, task__title__date, task__title, task__date, task__actions } from '../../styles/todo.module.scss';
 
+// Apollo
+import { gql, useMutation } from "@apollo/client";
+
 const Task = ({ task }) => {
+
+	const [ DeleteTask ] = useMutation(DeleteTask_MUTATION);
 
 	// Format the date
 	const createdAt = new Intl.DateTimeFormat('en-GB', { dateStyle: 'full' }).format(new Date(task.dateCreated));
+
+	const deleteMyTask = (e) => {
+		DeleteTask({ variables: { slug: e.slug } })
+	}
 
 	return (
 		<div className={ `card ${ task.complete && "complete" }` }>
@@ -16,7 +25,7 @@ const Task = ({ task }) => {
 					<small className={ task__date }><strong>Posted On:</strong> { createdAt }</small>
 				</div>
 				<div className={ task__actions }>
-					<button className="btn btn-outline-danger btn-sm">
+					<button className="btn btn-outline-danger btn-sm" onClick={ () => deleteMyTask(task) }>
 						<svg width="14" height="14" fill="currentColor" className="bi bi-trash" viewBox="0 0 16 16">
 							<path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
 							<path fillRule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
@@ -37,5 +46,15 @@ const Task = ({ task }) => {
 		</div>
 	)
 }
+
+export const DeleteTask_MUTATION = gql`
+	mutation DeleteTask($slug: String){
+		deleteTask(slug: $slug){
+			task{
+				id
+			}
+		}
+	}
+`
 
 export default Task
